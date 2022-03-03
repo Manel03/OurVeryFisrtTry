52a4cb93b4dfdb0bdab7dad9e9db5cf28f1d0fd2
oumayma.ayadi <oumayma.ayadi@esprit.tn>
2022-03-03 22:01:31 +0100

    commited by oumayma

---
+++ src/javaapplication1/InterfaceA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author oum
+ */
+public interface InterfaceA {
+    
+    void test();
+    int test2(int x);
+    
+}
---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,25 @@
+
+package javaapplication1;
+
+public class JavaApplication1 implements  InterfaceA, InterfaceB{
+
+    public static void main(String[] args) {
+       
+    }
+
+    @Override
+    public void test() {
+        System.out.println("kdejfhn");
+    }
+
+    @Override
+    public int test2(int x) {
+      return x;
+    }
+
+    @Override
+    public void tester() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
